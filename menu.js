ddda1f732e06f419529ac64bdc3b6db208322035
Vania1996 <jessivania.silva@hotmail.com>
2024-03-01 13:48:42 -0300

    Atualizei com o icone

--- menu.js
+++ menu.js
@@ -1,24 +1,25 @@
-var menu = `<div class="menu-degrade">
+var menu = `<!--  menu -->
+<div class="menu-degrade">
+<a href=""><img style="height: 80px;" src="logo/icon2.ico" alt=""></a>
 <div class="row">
     <div class="col">
         <a href="index.html">Inicio</a>
         <a href="quarto.html">Quarto</a>
         <a href="sala.html">Sala</a>
-        <a href="cozinha.html">Cozinhaa</a>
+        <a href="cozinha.html">Cozinha</a>
     </div>
+</div>
+</div>
 
-</div>
-</div>
+<!-- fim menu -->
 
 <style>
-
-/* menu */
 .menu-degrade{
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    background-image: linear-gradient(to left, rgb(14, 14, 20), rgb(151, 130, 91), rgb(84, 53, 53),rgb(41, 6, 6));
+    background-image: linear-gradient(to left, rgb(39, 67, 170), rgb(105, 102, 96), rgb(17, 52, 210),rgb(163, 125, 125));
     background-size: 500% 100%;
     animation: menu 5s infinite alternate;
     padding: 40px 4%;
@@ -37,9 +38,9 @@ var menu = `<div class="menu-degrade">
     }
 }
 
-/* fim menu */
-
 </style>
+
+
 `
 
 
