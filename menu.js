4844053f868d8f891d2d90eb1e38572dfe05f426
Vania1996 <jessivania.silva@hotmail.com>
2024-03-01 11:50:56 -0300

    Inclui novas paginas e mechi no arquivo quarto

--- menu.js
+++ menu.js
@@ -1,29 +1,49 @@
 var menu = `<div class="menu-degrade">
 <div class="row">
     <div class="col">
-        <a href="">Inicio</a>
-        <a href="">Quarto</a>
-        <a href="">Sala</a>
-        <a href="">Cozinhaa</a>
+        <a href="index.html">Inicio</a>
+        <a href="quarto.html">Quarto</a>
+        <a href="sala.html">Sala</a>
+        <a href="cozinha.html">Cozinhaa</a>
     </div>
 
 </div>
 </div>
 
 <style>
-<div class="menu-degrade">
-        <div class="row">
-            <div class="col">
-                <a href="">Inicio</a>
-                <a href="">Quarto</a>
-                <a href="">Sala</a>
-                <a href="">Cozinhaa</a>
-            </div>
 
-        </div>
-    </div>
+/* menu */
+.menu-degrade{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient(to left, rgb(14, 14, 20), rgb(151, 130, 91), rgb(84, 53, 53),rgb(41, 6, 6));
+    background-size: 500% 100%;
+    animation: menu 5s infinite alternate;
+    padding: 40px 4%;
+}
+.menu-degrade a{
+    color:rgb(255, 255, 255);
+    font-weight:bold;
+    margin: 10px;
+}
+@keyframes menu {
+    0%{
+        background-position-x:0%;
+    }
+    100%{
+        background-position-x:100% ;
+    }
+}
+
+/* fim menu */
+
 </style>
 `
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     // Aguarde o DOM ser completamente carregado
     document.querySelector(".menu").innerHTML = menu;
